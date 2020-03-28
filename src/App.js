790c0af69ee357b9d5ca067f42828718accac2e5
niminghui <877588092@qq.com>
2020-03-28 19:50:15 +0800

    less和antd配置好了

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import './App.css'
-
+import { Button } from 'antd'
 function App() {
   return (
     <div className="App">
-      <h1>odshfkjhdsuifhndishfoidshfoidshnf</h1>
+      <div>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+        <Button type="link">Link</Button>
+      </div>
     </div>
   )
 }
